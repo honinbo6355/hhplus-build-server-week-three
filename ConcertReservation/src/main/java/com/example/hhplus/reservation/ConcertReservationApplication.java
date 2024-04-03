b18a2db1c7e08a2a6ce1b3ffe0a1c8efe0a53b30
honinbo6355 <honinbo6355@nate.com>
2024-04-04 02:05:04 +0900

    initial commit

---
+++ ConcertReservation/src/main/java/com/example/hhplus/reservation/ConcertReservationApplication.java
@@ -0,0 +1,13 @@
+package com.example.hhplus.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertReservationApplication.class, args);
+	}
+
+}
